1942650063daa1a21786b9035dd1af6d77ac196d
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2023-08-23 17:25:51 +0900

    \omathchar etc.

--- texk/web2c/nptexdir/kanji.h
+++ texk/web2c/nptexdir/kanji.h
@@ -2,7 +2,11 @@
 #define KANJI_H
 #include "ptexenc/ptexenc.h"
 #include <ptexenc/unicode.h>
+
+#define getintone(w) ((w).cint1)
+#define setintone(w,a) ((w).cint1=(a))
+
+#define delcharfield(x) ((unsigned)(x) & 0x1FFFFF)
+#define delfamfield(x)  ((unsigned)(x)>>21)
 #endif
 
-extern integer calcpos (integer c);
-extern integer kcatcodekey (integer c);
