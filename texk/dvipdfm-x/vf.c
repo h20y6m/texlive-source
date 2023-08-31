db7b9ccde4e8474ef8d0325e6e17a5f176569e70
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2023-09-01 07:00:34 +0900

    dvipdfm-x/vf.c: vf fallback in xdv

--- texk/dvipdfm-x/vf.c
+++ texk/dvipdfm-x/vf.c
@@ -415,8 +415,7 @@ void vf_set_char(int32_t ch, int vf_font)
     if (ch >= vf_fonts[vf_font].num_chars ||
 	!(start = (vf_fonts[vf_font].ch_pkt)[ch])) {
       int is_jfm = tfm_is_jfm(vf_fonts[vf_font].dev_fonts[0].tfm_id);
-      if (is_jfm &&
-          ch < 0x1000000 && dpx_conf.compat_mode != dpx_mode_xdv_mode) {
+      if (is_jfm && ch < 0x1000000 ) {
         /* fallback multibyte character for (u)pTeX */
         if (dpx_conf.verbose_level == 1)
 	  if (vf_fonts[vf_font].message_flag == 0) {
